ce995a7ae8d17509b7ac416b76fd647a6b065409
Swen Kooij <swen@sectorlabs.ro>
2016-12-11 14:41:03 +0200

    Adapted to make run.sh work on MacOS

--- run.sh
+++ run.sh
@@ -1,1 +1,11 @@
-docker run -v $(pwd):/home/developer/image-converter -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=:0 -ti photonios/qtcpp:latest
+display=$DISPLAY
+socket="/tmp/.X11-unix"
+
+if [ "$(uname)" == "Darwin" ]; then
+    ip=10.200.10.1
+    sudo ifconfig lo0 alias $ip/24
+    display="$ip:0"
+fi
+
+
+docker run --privileged -v $(pwd):/home/developer/image-converter -v $socket:$socket -e DISPLAY=$display -ti photonios/qtcpp:latest
